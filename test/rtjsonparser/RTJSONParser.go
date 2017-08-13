1e6bc88e0e8176a3de6c6c62814de83bf6ba7037
Anirban Kundu <akundu@evernote.com>
2017-08-13 08:17:22 -0700

    just some tests

---
+++ test/rtjsonparser/RTJSONParser.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
+
+	"github.com/akundu/utilities/RTJobRunner"
+	"github.com/akundu/utilities/logger"
+)
+
+type worker struct {
+}
+
+func (this *worker) PreRun() {
+}
+func (this *worker) Run(id int, jobs <-chan RTJobRunner.Request, results chan<- RTJobRunner.Response) {
+	for j := range jobs {
+		job, ok := j.(string)
+		if ok == false {
+			results <- nil
+			continue
+		}
+		results <- job
+	}
+}
+
+func main() {
+	jh := RTJobRunner.NewJobHandler(*num_simultaneously_to_run_ptr, &worker{}, false)
+	if err := jh.ProcessJobsFromJSON(*json_file_ptr); err != nil {
+		logger.Error.Println(err)
+		return
+	}
+	//mark that there are no more jobs to add
+	jh.DoneAddingJobs()
+	//wait for the results
+	jh.WaitForJobsToComplete()
+
+	//print results
+	for i := range jh.Results {
+		if r, ok := jh.Results[i].(string); ok {
+			fmt.Println(r)
+		}
+	}
+}
+
+var (
+	num_simultaneously_to_run_ptr = flag.Int("p", 1, "num times to run action")
+	print_results_ptr             = flag.Int("v", 0, "print results")
+	json_file_ptr                 = flag.String("f", "", "json file to load jobs from")
+)
+
+func init() {
+	logger.DefaultLoggerInit()
+	logger.Init(ioutil.Discard, os.Stdout, ioutil.Discard, os.Stderr)
+	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+
+	flag.Parse()
+}
